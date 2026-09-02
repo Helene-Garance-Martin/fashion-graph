deca6c33d0ef96561d12fa62100c054818081b20
Helene-MartinROH <helene.chevreuil.martin@gmail.com>
2026-09-02 15:38:02 +0100

    style: establish typography system

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "@fontsource/tenor-sans";
+import "@fontsource/outfit/300.css";
+import "@fontsource/outfit/500.css";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
